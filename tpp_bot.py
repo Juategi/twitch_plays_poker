a36874ebc5d49ec6a3d80ae791e3fbd89bde6b69
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2024-01-26 05:27:13 +0100

    update functions

--- tpp_bot.py
+++ tpp_bot.py
@@ -22,6 +22,9 @@ CHANNEL = "twitchplayspokerstars"
 OWNER = "twitchplayspokerstars"
 
 mode_image = 'assets/mode.png'
+turn_image = 'assets/your_turn.png'
+play_image = 'assets/play_now.png'
+confirm_image = 'assets/confirm.png'
 
 message = ""
 user = ""
@@ -45,6 +48,7 @@ def joinchat():
 		for line in readbuffer_join.split("\n")[0:-1]:
 			print(line)
 			Loading = loadingComplete(line)
+	irc.send("CAP REQ :twitch.tv/tags\r\n".encode())
 
 def loadingComplete(line):
 	if("End of /NAMES list" in line):
@@ -78,14 +82,28 @@ def getMessage(line):
 		messages['!call'] += 1
 	elif message == "!allin":
 		messages['!allin'] += 1
+	elif  message == "!raise1":
+		messages['!raise1'] += 1
+	elif message == "!raise2":
+		messages['!raise2'] += 1
+	elif message == "!raise3":
+		messages['!raise3'] += 1
+	elif message == "!raise4":
+		messages['!raise4'] += 1
+	elif message == "!raise5":
+		messages['!raise5'] += 1
+	elif message == "!raise6":
+		messages['!raise6'] += 1
+	elif message == "!raise7":
+		messages['!raise7'] += 1
+	elif message == "!raise8":
+		messages['!raise8'] += 1
+	elif message == "!raise9":
+		messages['!raise9'] += 1
 	elif message == "!raise10":
 		messages['!raise10'] += 1
-	elif message == "!raise25":
-		messages['!raise25'] += 1
-	elif message == "!raise50":
-		messages['!raise50'] += 1
-	elif message == "!raise75":
-		messages['!raise75'] += 1
+	elif message == "!raise15":
+		messages['!raise15'] += 1
 	return message
 
 
@@ -94,10 +112,18 @@ def clearCommands():
 	messages['!fold'] = 0
 	messages['!call'] = 0
 	messages['!allin'] = 0
+	messages['!raise1'] = 0
+	messages['!raise2'] = 0
+	messages['!raise3'] = 0
+	messages['!raise4'] = 0
+	messages['!raise5'] = 0
+	messages['!raise6'] = 0
+	messages['!raise7'] = 0
+	messages['!raise8'] = 0
+	messages['!raise9'] = 0
 	messages['!raise10'] = 0
-	messages['!raise25'] = 0
-	messages['!raise50'] = 0
-	messages['!raise75'] = 0
+	messages['!raise15'] = 0
+
 	users.clear()
 
 def votation():
@@ -107,27 +133,43 @@ def votation():
 		if value > max:
 			max = value
 			maxCommand = key
+	if max == 0:
+		return ""
 	sendMessage(irc, "The next command is: " + maxCommand)
 	clearCommands()
 	return maxCommand
 
 def executeCommand(command):
 	if command == '!check':
-		pyautogui.press('p')
+		pyautogui.press('c')
 	elif command == '!fold':
 		pyautogui.press('f')
 	elif command == '!call':
 		pyautogui.press('i')
 	elif command == '!allin':
 		pyautogui.press('a')
-	elif command == '!raise10':
+	elif command == '!raise1':
 		pyautogui.press('1')
-	elif command == '!raise25':
+	elif command == '!raise2':
 		pyautogui.press('2')
-	elif command == '!raise50':
+	elif command == '!raise3':
+		pyautogui.press('3')
+	elif command == '!raise4':
+		pyautogui.press('4')
+	elif command == '!raise5':
 		pyautogui.press('5')
-	elif command == '!raise75':
+	elif command == '!raise6':
+		pyautogui.press('6')
+	elif command == '!raise7':
 		pyautogui.press('7')
+	elif command == '!raise8':
+		pyautogui.press('8')
+	elif command == '!raise9':
+		pyautogui.press('9')
+	elif command == '!raise10':
+		pyautogui.press('0')
+	elif command == '!raise15':
+		pyautogui.press('-')
 
 def find_image_on_screen(target_image_path, confidence_threshold=0.8):
     target_image = cv2.imread(target_image_path)
@@ -169,42 +211,47 @@ def find_and_click_image(target_image_path, confidence_threshold=0.8):
             break
         
         time.sleep(1)
+
+
+def getMessages():
+	while True:
+			try:
+				readbuffer = irc.recv(1024).decode()
+			except:
+				readbuffer = ""
+			for line in readbuffer.split("\r\n"):
+				if line == "":
+					continue
+				if "PING :tmi.twitch.tv" in line:
+					#print(line)
+					msgg = "PONG :tmi.twitch.tv\r\n".encode()
+					irc.send(msgg)
+					#print(msgg)
+					continue
+				else:
+					try:
+						user = getUser(line)
+						message = getMessage(line)		
+						print(user + " : " + message)														
+						#print(messages)
+						executeCommand(votation())
+						if(user not in users):
+							users.append(user)
+							print(users)
+							#message = getMessage(line)											
+							#print(messages)
+					except Exception:
+						print("Error")
 	
 
 def twitch():	
 	joinchat()
 	clearCommands()
-	print(messages)
-	irc.send("CAP REQ :twitch.tv/tags\r\n".encode())
-	while True:
-		try:
-			readbuffer = irc.recv(1024).decode()
-		except:
-			readbuffer = ""
-		for line in readbuffer.split("\r\n"):
-			if line == "":
-				continue
-			if "PING :tmi.twitch.tv" in line:
-				#print(line)
-				msgg = "PONG :tmi.twitch.tv\r\n".encode()
-				irc.send(msgg)
-				#print(msgg)
-				continue
-			else:
-				try:
-					user = getUser(line)
-					message = getMessage(line)		
-					print(user + " : " + message)														
-					print(messages)
-					if(user not in users):
-						users.append(user)
-						print(users)
-						#message = getMessage(line)											
-						#print(messages)
-				except Exception:
-					print("Error")
+	getMessages()
+	
+	
 
 def main():
 	if __name__ =='__main__':
-		find_and_click_image(mode_image)
+		find_and_click_image(turn_image)
 main()
