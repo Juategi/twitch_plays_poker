08372ad228a8855a49f73510e28ef87c3a388ce9
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2024-01-27 04:08:08 +0100

    start game done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twitch_plays_poker
  
+contador de total ganado,
+movimientos hechos en pantalla
+comprobar dilucion de raises
--- tpp_bot.py
+++ tpp_bot.py
@@ -25,6 +25,7 @@ mode_image = 'assets/mode.png'
 turn_image = 'assets/your_turn.png'
 play_image = 'assets/play_now.png'
 confirm_image = 'assets/confirm.png'
+test_image = 'assets/test.png'
 
 message = ""
 user = ""
@@ -171,18 +172,7 @@ def executeCommand(command):
 	elif command == '!raise15':
 		pyautogui.press('-')
 
-def find_image_on_screen(target_image_path, confidence_threshold=0.8):
-    target_image = cv2.imread(target_image_path)
-    screen = np.array(ImageGrab.grab())
-    result = cv2.matchTemplate(screen, target_image, cv2.TM_CCOEFF_NORMED)
-    
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-    if max_val >= confidence_threshold:
-        return True
-    else:
-        return False
-	
 def find_and_click_image(target_image_path, confidence_threshold=0.8):
     target_image = cv2.imread(target_image_path)
     
@@ -206,11 +196,49 @@ def find_and_click_image(target_image_path, confidence_threshold=0.8):
             # Click on the center of the found image
             pyautogui.click(center_x, center_y)
 
-            print("¡Imagen encontrada y clic realizada!")
+            print("¡Imagen encontrada " + target_image_path)
             # Puedes realizar acciones adicionales aquí si es necesario
             break
         
         time.sleep(1)
+
+
+def find_and_move_image(target_image_path, confidence_threshold=0.8):
+    target_image = cv2.imread(target_image_path)
+    
+    while True:
+        screen = np.array(ImageGrab.grab())
+        result = cv2.matchTemplate(screen, target_image, cv2.TM_CCOEFF_NORMED)
+        
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        if max_val >= confidence_threshold:
+            # Get the coordinates of the top-left corner of the found image
+            top_left_x, top_left_y = max_loc
+
+            # Get the width and height of the found image
+            h, w = target_image.shape[:2]
+
+            # Calculate the center coordinates of the found image
+            center_x = top_left_x + w // 2
+            center_y = top_left_y + h // 2
+
+            # Click on the center of the found image
+            pyautogui.moveTo(center_x, center_y)
+
+            print("¡Imagen encontrada " + target_image_path)
+            # Puedes realizar acciones adicionales aquí si es necesario
+            break
+        
+        time.sleep(1)
+
+
+def startGame():
+	find_and_click_image(mode_image)
+	time.sleep(1)
+	find_and_click_image(play_image)
+	time.sleep(1)
+	find_and_move_image(confirm_image)
 
 
 def getMessages():
@@ -253,5 +281,5 @@ def twitch():
 
 def main():
 	if __name__ =='__main__':
-		find_and_click_image(turn_image)
+		startGame()
 main()
